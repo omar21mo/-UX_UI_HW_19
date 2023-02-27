5174cd070b843b39e77002d39178a6b1c08c4fd4
omar21mo <110702360+omar21mo@users.noreply.github.com>
2023-02-26 23:38:20 -0500

    final

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("Your index.js file is loaded correctly chch")
+$("button").hover(function(){
+    $(this).css("background-color", "#0050F5"); 
+    $(this).css("color", "#D9D9D9");
+}, function(){
+    $(this).css("background-color","#FF7955");
+    $(this).css("color", "#12273F");
+});
